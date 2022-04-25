abedb6a1b368c81ebbc6d7887d20f3e382edb296
Chandra Burnham <burnhc@cs.washington.edu>
2022-04-25 00:27:09 -0700

    Learned to use graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,7 +13,7 @@ const config: GatsbyConfig = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `./blog`,
       }
     },
   ]
