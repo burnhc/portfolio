fa52b6391c880c7833d81e907a4f1dbd847e3363
Chandra Burnham <burnhc@cs.washington.edu>
2022-04-25 00:04:25 -0700

    Adding blogs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,10 +2,21 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `portfolio`,
+    title: `Chandra Burnham`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-react-helmet"]
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ]
 };
 
 export default config;
